59591a63d1ce8e86f01b00929bff833f670ba56d
myspc-development <myspchosting@gmail.com>
2025-06-13 19:10:14 +0300

    Refactor RSVP endpoint tests

--- tests/RsvpEndpointTest.php
+++ tests/RsvpEndpointTest.php
@@ -12,39 +12,39 @@ class RsvpEndpointTest extends TestCase
         Stubs::$posts = [(object)['ID' => 5, 'post_type' => 'ead_event']];
     }
 
-    public function test_toggle_rsvp_adds_id()
+    public function test_add_rsvp_adds_id()
     {
         $endpoint = new RsvpEndpoint();
         $request = new WP_REST_Request();
         $request->set_param('event_id', 5);
 
-        $response = $endpoint->toggle_rsvp($request);
+        $response = $endpoint->add_rsvp($request);
 
         $this->assertSame('added', $response->data['status']);
         $this->assertSame([5], Stubs::$user_meta['ead_rsvps']);
     }
 
-    public function test_toggle_rsvp_removes_existing()
+    public function test_remove_rsvp_removes_existing()
     {
         Stubs::$user_meta = ['ead_rsvps' => [5]];
         $endpoint = new RsvpEndpoint();
         $request = new WP_REST_Request();
         $request->set_param('event_id', 5);
 
-        $response = $endpoint->toggle_rsvp($request);
+        $response = $endpoint->remove_rsvp($request);
 
         $this->assertSame('removed', $response->data['status']);
         $this->assertSame([], Stubs::$user_meta['ead_rsvps']);
     }
 
-    public function test_toggle_rsvp_invalid_event()
+    public function test_add_rsvp_invalid_event()
     {
         Stubs::$posts = [];
         $endpoint = new RsvpEndpoint();
         $request = new WP_REST_Request();
         $request->set_param('event_id', 99);
 
-        $response = $endpoint->toggle_rsvp($request);
+        $response = $endpoint->add_rsvp($request);
 
         $this->assertSame(400, $response->status);
         $this->assertArrayHasKey('error', $response->data);
