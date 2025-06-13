752b63a85451185f9bb797a3846b3473c91a34e2
myspc-development <myspchosting@gmail.com>
2025-06-13 16:38:31 +0300

    Add Babel transpilation support

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
 };
