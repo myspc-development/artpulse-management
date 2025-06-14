a5cd9a11cd8dedd784320d9a3e9eb949939038c1
myspc-development <myspchosting@gmail.com>
2025-06-14 06:49:25 +0300

    Store membership status and assign roles

--- src/Shortcodes/MembershipSignupForm.php
+++ src/Shortcodes/MembershipSignupForm.php
@@ -3,8 +3,32 @@ namespace EAD\Shortcodes;
 
 class MembershipSignupForm {
     public static function register() {
-        add_shortcode('ead_membership_status', [self::class, 'render']);
-        add_action('wp_loaded', [self::class, 'handle_submit']);
+        add_shortcode( 'ead_membership_status', [ self::class, 'render' ] );
+        add_action( 'wp_loaded', [ self::class, 'handle_submit' ] );
+    }
+
+    /**
+     * Assign a membership role to the given user based on the level.
+     *
+     * @param int    $user_id User ID.
+     * @param string $level   Membership level slug.
+     */
+    private static function assign_role( $user_id, $level ) {
+        $user = new \WP_User( $user_id );
+
+        switch ( $level ) {
+            case 'basic':
+                $user->set_role( 'member_basic' );
+                break;
+            case 'pro':
+                $user->set_role( 'member_pro' );
+                break;
+            case 'org':
+                $user->set_role( 'member_org' );
+                break;
+            default:
+                $user->set_role( 'member_registered' );
+        }
     }
 
     public static function render() {
@@ -28,28 +52,26 @@ class MembershipSignupForm {
     }
 
     public static function handle_submit() {
-        if (empty($_POST['ead_join_membership'])) {
+        if ( empty( $_POST['ead_join_membership'] ) ) {
             return;
         }
-        if (empty($_POST['ead_membership_nonce']) || !wp_verify_nonce($_POST['ead_membership_nonce'], 'ead_membership_join_action')) {
+        if ( empty( $_POST['ead_membership_nonce'] ) || ! wp_verify_nonce( $_POST['ead_membership_nonce'], 'ead_membership_join_action' ) ) {
             return;
         }
-        if (!is_user_logged_in()) {
-            return;
-        }
-
-        $level = sanitize_text_field($_POST['membership_level'] ?? '');
-        $mapping = [
-            'basic' => 'member_basic',
-            'pro'   => 'member_pro',
-            'org'   => 'member_org',
-        ];
-
-        if (!isset($mapping[$level])) {
+        if ( ! is_user_logged_in() ) {
             return;
         }
 
-        $user = wp_get_current_user();
-        $user->set_role($mapping[$level]);
+        $level = sanitize_text_field( $_POST['membership_level'] ?? '' );
+
+        $uid = get_current_user_id();
+
+        update_user_meta( $uid, 'is_member', '1' );
+        update_user_meta( $uid, 'membership_level', $level );
+
+        self::assign_role( $uid, $level );
+
+        wp_redirect( add_query_arg( 'joined', '1', wp_get_referer() ) );
+        exit;
     }
 }
