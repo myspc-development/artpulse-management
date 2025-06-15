9fd15021d91836fb36a6f207b4e37477749ef938
myspc-development <myspchosting@gmail.com>
2025-06-15 10:36:44 +0300

    fix fatal error due to typed property override

--- src/Rest/ManageMembersEndpoint.php
+++ src/Rest/ManageMembersEndpoint.php
@@ -7,8 +7,8 @@ use WP_REST_Request;
 use WP_REST_Response;
 
 class ManageMembersEndpoint extends WP_REST_Controller {
-    protected string $namespace = 'artpulse/v1';
-    protected string $rest_base = 'manage-members';
+    protected $namespace = 'artpulse/v1';
+    protected $rest_base = 'manage-members';
 
     public function __construct() {
         add_action( 'rest_api_init', [ $this, 'register_routes' ] );
