f94df8493d4144182dd392d156afb504248e75bd
myspc-development <myspchosting@gmail.com>
2025-06-19 08:59:09 +0300

    Update CPT slugs from ead_* to artpulse_*

--- src/Admin/AdminListSorting.php
+++ src/Admin/AdminListSorting.php
@@ -6,22 +6,22 @@ class AdminListSorting {
         add_action('pre_get_posts', [self::class, 'sort_admin_columns']);
 
         // Make columns sortable
-        add_filter('manage_edit-ead_artist_sortable_columns', function ($columns) {
+        add_filter('manage_edit-artpulse_artist_sortable_columns', function ($columns) {
             $columns['artist_featured'] = 'artist_featured';
             return $columns;
         });
 
-        add_filter('manage_edit-ead_artwork_sortable_columns', function ($columns) {
+        add_filter('manage_edit-artpulse_artwork_sortable_columns', function ($columns) {
             $columns['artwork_featured'] = 'artwork_featured';
             return $columns;
         });
 
-        add_filter('manage_edit-ead_event_sortable_columns', function ($columns) {
+        add_filter('manage_edit-artpulse_event_sortable_columns', function ($columns) {
             $columns['event_featured'] = 'event_featured';
             return $columns;
         });
 
-        add_filter('manage_edit-ead_organization_sortable_columns', function ($columns) {
+        add_filter('manage_edit-artpulse_org_sortable_columns', function ($columns) {
             $columns['ead_org_type'] = 'ead_org_type';
             return $columns;
         });
